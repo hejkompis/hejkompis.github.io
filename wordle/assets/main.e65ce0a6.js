dc2376eb5f9b040727d33b302d2ad7c3ae96ff69
Per <per.olsson@gmail.com>
2022-02-20 01:01:57 +0100

    Update main.e65ce0a6.js

--- wordle/assets/main.e65ce0a6.js
+++ wordle/assets/main.e65ce0a6.js
@@ -331,7 +331,7 @@ this.wordle = this.wordle || {}, this.wordle.bundle = function(e) {
 
     function $a(e) { var a = ["th", "st", "nd", "rd"],
             s = e % 100; return e + (a[(s - 20) % 10] || a[s] || a[0]) }
-    var Ha = new Date(2022, 2, 20, 0, 0, 0, 0);
+    var Ha = new Date(2022, 1, 20, 0, 0, 0, 0);
 
     function Na(e, a) { var s = new Date(e),
             t = new Date(a).setHours(0, 0, 0, 0) - s.setHours(0, 0, 0, 0); return Math.round(t / 864e5) }
